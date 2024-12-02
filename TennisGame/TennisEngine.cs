fdb27b04e69760937732dbd7fe54d48cdf20b4bf
Pikahsiu <a12345a12001@gmail.com>
2024-12-02 13:42:47 +0800

    Pass FifteenLove Test (The code is clean so there is no need to refactor it.)

--- TennisGame/TennisEngine.cs
+++ TennisGame/TennisEngine.cs
@@ -2,13 +2,19 @@
 
 public class TennisEngine
 {
+    private int _firstPlayerScoreTimes;
+
     public string Score()
     {
+        if (_firstPlayerScoreTimes == 1)
+        {
+            return "Fifteen Love";
+        }
         return "Love All";
     }
 
     public void FirstPlayerScore()
     {
-        throw new NotImplementedException();
+        _firstPlayerScoreTimes++;
     }
 }
